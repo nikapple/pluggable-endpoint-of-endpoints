7e9c2b4946a5a6d92e77a2e83b506bdf3eea4242
Nikhil Pereira <nikhil.pereira@ideacrestsolutions.com>
2017-11-22 14:26:50 +0530

    pluggable endpoint controller web fragment coded

---
+++ src/main/java/com/ideacrest/endpointapp/controller/EndpointController.java
@@ -0,0 +1,26 @@
+package com.ideacrest.endpointapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+@Controller
+public class EndpointController {
+	
+	private final RequestMappingHandlerMapping handlerMapping;
+
+	@Autowired
+	public EndpointController(RequestMappingHandlerMapping handlerMapping) {
+		this.handlerMapping = handlerMapping;
+	}
+	
+	@RequestMapping(value="/endpointdoc", method=RequestMethod.GET)
+	public String show(Model model) {
+		model.addAttribute("handlerMethods", this.handlerMapping.getHandlerMethods());
+		return "endpoints";
+	}
+	
+}
